7f09e63a7b08c881dd58d85d66b3ec06c8882f1e
rener <rener.oliveira@fgv.edu.br>
2020-05-09 21:53:20 -0300

    atualizando resumo e criando removedor de acentos

--- Pré-Processamento/get_words.py
+++ Pré-Processamento/get_words.py
@@ -1,12 +1,48 @@
+#!/usr/local/bin/python
+# coding: latin-1
+import os
+import re
+import unicodedata as ud
 
-def get_articles(n,file):
+def get_articles(file,n): #pega uma amostra de n domumentos em "file"
+    counts=0
+    end="ENDOFARTICLE"
+    out=open('sample.txt',"w+")
+    with open(file,"r") as f:
+        lines=f.readlines()
+        while counts<=n:
+            for line in lines:
+                out.write(line)
+                if line[0:12]==end:
+                    counts +=1
+    out.close()
+def remove_accents(string):
+    if type(string) is not unicode:
+        string = unicode(string, encoding='utf-8')
 
+    string = re.sub(u"[ÁÀÂÃÄÅàáâãäå]", 'a', string)#ª
+    string = re.sub(u"[ÈÉÊËẼẽèéêë]", 'e', string)
+    string = re.sub(u"[ÍÌÏĨÎĩîìíîï]", 'i', string)
+    string = re.sub(u"[ÒÓÔÖÕõòóôõö]", 'o', string)
+    string = re.sub(u"[ŨÙÚÛÜũùúûü]", 'u', string)
+    string = re.sub(u"[ÝỲỸŸŶỳŷỹýÿ]", 'y', string)
+    string = re.sub(u"[Ññ]", 'n', string)
+    string = re.sub(u"[¢çĉćĉCĈĆĈ]", 'c', string)
+    string = re.sub(u"[Ž]", 'z', string)
+
+    return string
 
 def main():
-    print("hello")
-    
+    #get_articles("raw_0_10000",5)
 
+    sample = open('sample.txt', 'r') #arquivo original
+    result = open('result_tests.txt', 'w') #abrir o arquivo em modo leitura
+   
 
-
+    #for line in sample:
+     #       words = re.findall('[a-zA-Z0-9]+',line)
+      #      line = (" ").join(words)
+    sample.close()
+    result.close()
 if __name__=="__main__":
     main()
--- Pré-Processamento/resumo.md
+++ Pré-Processamento/resumo.md
@@ -1,5 +1,7 @@
 # Dinâmica do pré-processamento:
 
 - Gerar um (ou vários) arquivo(s) contendo as palavras únicas, os respectivos documentos em que ela aparece, e a frequência em que aparece. (Esse passo vai faciliar o processo de inserção na árvore, e guardar a frequência de repetições ajudará num possível rankeamento de páginas)
+
+essa lista terá apenas palavras sem acentos, com letras minúsculas e considerará apenas as 26 letras do alfabeto inglês mais os 10 algarismos.
 - Gerar um arquivo que associa os ID's dos documentos aos títulos (para exibir os resultados finais no terminal)
 - Organizar os arquivos brutos, de forma que tendo o ID do documento, retornar o conteúdo seja uma tarefa rápida. (possivelmente faremos um arquivo para cada documento e os organizaremos em pastas com uma quantidade fixa deles)
