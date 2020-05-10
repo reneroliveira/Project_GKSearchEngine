50cbf75e01470a35a060babe9e8bf75bfd7fb973
rener <rener.oliveira@fgv.edu.br>
2020-05-10 10:56:50 -0300

    extração de títulos e index concluida

--- Pré-Processamento/get_words.py
+++ Pré-Processamento/get_words.py
@@ -5,20 +5,23 @@ import re
 import unicodedata as ud
 
 def get_articles(file,n): #pega uma amostra de n domumentos em "file"
-    counts=0
-    end="ENDOFARTICLE"
+    
     out=open('sample.txt',"w+")
     with open(file,"r") as f:
         lines=f.readlines()
-        while counts<=n:
-            for line in lines:
-                out.write(line)
-                if line[0:12]==end:
-                    counts +=1
+        counts=0
+        
+        for line in lines:
+            if counts>=n:
+                break
+            out.write(line)
+            if re.match("ENDOFARTICLE",line):
+                print(line)
+                counts +=1
     out.close()
 def remove_accents(string):
-    if type(string) is not unicode:
-        string = unicode(string, encoding='utf-8')
+    #if type(string) is not unicode:
+        #string = unicode(string, encoding='utf-8')
     
     string = re.sub(u"[ÁÀÂÃÄÅàáâãäå]", 'a', string)#ª
     string = re.sub(u"[ÈÉÊËẼẽèéêë]", 'e', string)
@@ -31,27 +34,34 @@ def remove_accents(string):
     string = re.sub(u"[Ž]", 'z', string)
     return string #.encode('ascii','ignore')
 
+
+def get_words(file):
+    with open(file, 'r',encoding="latin-1") as sample: 
+        result = open('result_tests.txt', 'w+')
+        titles = open('titles.txt','w+')
+
+        for line in sample:
+            line=remove_accents(line)
+            line = (" ").join(re.findall('\w+',line))
+            aux=[]
+            if re.match("doc",line) and len(line)>3:#linha que contem as informações
+                title=re.search("title\s?(\w+\s+)+nonfiltered",line).group()[6:-11].upper()
+                index=re.search("dbindex\s?\d+\s?",line).group()[8:]
+                x=title.lower().split()
+                for i in x:
+                    if i is not in
+                titles.write(index+","+title+"\n")
+            elif (re.match("doc",line) and len(line)<=3) or re.match("ENDOFARTICLE",line):
+                #result.write("\n")
+                pass
+            else:
+                pass
+        result.close()
+        titles.close()
 def main():
     #get_articles("raw_0_10000",5)
-
-    sample = open('sample.txt', 'r') #arquivo original
-    result = open('result_tests.txt', 'w') #abrir o arquivo em modo leitura
-   
-
-    for line in sample:
-        if line[0:4]=="<doc":
-            title=re.search("title=\"(\w+\s)+\w+\"",line)
-            title=title.group()[7:-1]
-            id=re.search("id=\"(\d+)\"",line)
-            id=id.group()[4:-1]
-            print(id,title)
-        #if line[0:4]!="<doc" and line[0:6]!="</doc>" and line[0:12]!="ENDOFARTICLE":
-         #   words = re.findall('[a-zA-Z0-9]+',line)
-          #  line = (" ").join(words)
-    st=re.search("\"(\w+\s)+\w+\"", "title=\"rener oliveira da silva\" nonfiltered=\"1\" proc")
-    print(st.group()[3:-1])
-    result.close()
-    sample.close()
+    get_words('sample.txt')
+    
 if __name__=="__main__":
-    get_articles("raw_0_10000",5)
+    #get_articles("raw_0_10000",5)
     main()
