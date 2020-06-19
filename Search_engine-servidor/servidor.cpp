a2a335f239897a0c93b6578fed4666d223f6c05e
reneroliveira <53785783+reneroliveira@users.noreply.github.com>
2020-06-18 21:14:32 -0300

    atualizacao server2

--- Search_engine-servidor/servidor.cpp
+++ Search_engine-servidor/servidor.cpp
@@ -16,26 +16,87 @@ using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
 
 
+Trie GKSE;
+void start(){
+	inicializa(GKSE);
+}
+
 
 int main(){
-        
+	start();
     
 //sabemos que it->second armazena nossa palavra do formulario e 
 // stream << "{\"res\":\"" <<  it->second << img_button << "\"}"; é responsavel por mostra ela
 	HttpServer server;
 	server.config.port = 8080; //porta do servidor será localhost/8080
-
+	//server.init
 	//função que recebe query do tipo localhost:8080/query?text={text}
 	server.resource["^/query$"]["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request){
 		//Trecho do código para obter a query (consulta)
+		
 		auto query_fields = request->parse_query_string(); //Recebe o conteúdo de "entrada"
 		auto it = query_fields.find("text"); // Obtem o texto do conteudo
 		cout << it->second << endl;
+<<<<<<< HEAD
+		
+		
+    
+		
+=======
+>>>>>>> 7c3a0284017973e8dbc0a4f5b6eb6f33c41f2400
 		//A resposta para o servidor vai ser um JSON do formato
 		//{"res": "O seu projeto final vai ser incrível, [texto]!"}	
 		//JSON
 		// {"atributo1":"valor1",
 		//	"atributo2": {"sub-atributo": "valor2"}} 
+<<<<<<< HEAD
+		string img_button = "</br> <input id = 'btn_img' type = 'button' onclick='print_img()' value ='Me clique!'> </br>";
+		vector<std::string> resultado;
+		stringstream res;
+               vector<Pair> rest = pesquisa(it->second,GKSE); 
+               int tamanho = rest.size();
+               ifstream arquivoE;
+               string linha;
+			   string preffix="./../aNames/";
+			  
+     /////////////////////////////////////          //função find + transforma em string
+                for(int k = 0;k<min(20,tamanho);k++){//só pega 20 resultados
+					int number = rest[k][0] / 10000;
+					 string name = "aTitles_" + to_string(number*10000) + "_" + to_string((number+1)*10000) + ".txt";
+               arquivoE.open(preffix+name);
+                int i = 0;
+                int numero = 0;
+                if(arquivoE.is_open()){
+              while(getline(arquivoE,linha)){
+					//aqui podemos fazer operaões linha a linha
+					stringstream geek(linha);
+					geek >> numero;
+					
+					for(int j  = 0;j<tamanho;j++){
+						if(rest[j][0] == numero){
+							resultado.push_back(linha);
+						}
+					}            
+                }
+  			}
+				};
+  //////////////////////////////////////////////////////
+  //resultado =string com os titulos
+               /*std::string resultado = " ";
+               resultado = resultado +std::to_string(rest[0]); */
+                if(it->second == "sleep"){
+		res << "{\"res\":\"" << resultado[0] << "\"}";
+		response->write(res);
+		}
+		
+		else{ 
+		for(int k=0;k<resultado.size();k++){
+		res << "{\"res\":\"" << resultado[k] <<"</BR>"<< "\"}";
+		}
+		cout << res.str();
+		response->write(res);
+		}
+=======
 		string img_button = "</br> <input id = 'btn_img' type = 'button' onclick='print_img()' value ='Me clique!'> </br>";	        vector<string> resultado;
 		stringstream res;
                vector<int> rest = pesquisa(it->second);
@@ -85,6 +146,7 @@ int main(){
 	       
 	       
 		
+>>>>>>> 7c3a0284017973e8dbc0a4f5b6eb6f33c41f2400
 	};
 	
 	
